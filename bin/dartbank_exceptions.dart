0211812e4b09a64c5a6328e6228ed0a51d068674
Thiago Elias <thiagoelias99@gmail.com>
2023-03-06 09:55:49 -0300

    project created

---
+++ bin/dartbank_exceptions.dart
@@ -0,0 +1,5 @@
+import 'package:dartbank_exceptions/dartbank_exceptions.dart' as dartbank_exceptions;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartbank_exceptions.calculate()}!');
+}
---
+++ test/dartbank_exceptions_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartbank_exceptions/dartbank_exceptions.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
